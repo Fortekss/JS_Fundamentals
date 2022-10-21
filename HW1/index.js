f938b700fa53be4f6096abd7db46a7daf21ee132
Fortekss <116352643+Fortekss@users.noreply.github.com>
2022-10-21 16:40:35 +0300

    Add files via upload

---
+++ HW1/index.js
@@ -0,0 +1,52 @@
+console.log("Kozak");                                              //task 2
+
+let first = "1"                                                    //task 3
+let second = "2"
+alert (first + ' and ' + second)
+first = second 
+alert (first + ' and ' + second)               
+                                                                                
+const object = {                                                  //task 4                                                                    
+avaible : "yes",
+capacity : "30",
+licenced : "true",
+age : "null",
+adress : "undefined",
+} 
+console.log(object)
+
+
+let isAdult = confirm("To continue,your age must be more than 18") ;  //task 5
+console.log(isAdult);
+ 
+let myname = "Andrii" ; //string                                      //task 6
+let surname = "Kozak" ; //string
+let group = "1" ; //number
+let birthyear = "2004" ; //number
+let ismarried = "false" ; //boolean
+console.log('name is ' + typeof myname) ;
+console.log('surname is ' + typeof surname) ;
+console.log('group is ' + typeof 1) ;
+console.log('birthyear is ' + typeof 2004) ;
+console.log('ismarried is ' + typeof false) ;
+console.log(group,birthyear,ismarried,myname,surname,) ;
+
+let login = prompt('Please,enter your Nickname') ;                    //task 7
+let mail = prompt('Please,enter your E-mail') ;
+let password = prompt('Please,enter your Password') ;
+alert ('Dear '+ login + ' your e-mail is ' + mail + ' and your password is ' + password) ;
+
+let SecInMinute = 60 ;                                                 //task 8
+let SecInHour = SecInMinute * 60 ;
+let SecInDay = SecInHour * 24 ;
+let Months = prompt ('Enter the number of days in the month') ;
+let SecInMonth = Months * SecInDay ;
+alert('There is ' + SecInHour + ' seconds in the hour, ' + SecInDay + ' in the day and ' + SecInMonth + ' in the month.')
+
+
+
+
+
+                            
+
+
