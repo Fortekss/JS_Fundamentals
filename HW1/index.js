63b9c1dd69377698dacea10aa710fbded73bc09e
Fortekss <116352643+Fortekss@users.noreply.github.com>
2022-10-24 18:43:45 +0300

    Update index.js

--- HW1/index.js
+++ HW1/index.js
@@ -5,34 +5,41 @@ let second = "2"
 alert (first + ' and ' + second)
 first = second 
 alert (first + ' and ' + second)               
-                                                                                
-const object = {                                                  //task 4                                                                    
-avaible : "yes",
-capacity : 30,
-licenced : true,
-age : null,
-adress : undefined,
-} 
-console.log(avaible) ;
-console.log(capacity) ;
-console.log(licenced) ;
-console.log(age) ;
-console.log(adress) ;
+
+let object = {                                                    //task 4
+    str: "object", 
+    age: 2,
+    avaible: true,
+    color: undefined,
+    adress: null
+};
+
+console.log(object);
+
+console.log(typeof object.str);
+console.log(typeof object.age);
+console.log(typeof object.avaible);
+console.log(typeof object.color);
+console.log(typeof object.adress) ;
+
 
 let isAdult = confirm("To continue,your age must be more than 18") ;  //task 5
 console.log(isAdult);
  
-let myname = "Andrii" ; //string                                      //task 6
-let surname = "Kozak" ; //string
-let group = "1" ; //number
-let birthyear = "2004" ; //number
-let ismarried = "false" ; //boolean
-console.log('name is ' + typeof myname) ;
-console.log('surname is ' + typeof surname) ;
-console.log('group is ' + typeof 1) ;
-console.log('birthyear is ' + typeof 2004) ;
-console.log('ismarried is ' + typeof false) ;
-console.log(group,birthyear,ismarried,myname,surname,) ;
+const me = {                                                 //task 6
+myname: "Andrii", //string                                     
+surname: "Kozak" , //string
+studygroup: 1 , //number
+birthyear :2004 , //number
+ismarried: false , //boolean
+}
+console.log('name is ' + typeof me.myname) ;
+console.log('surname is ' + typeof me.surname) ;
+console.log('group is ' + typeof me.studygroup) ;
+console.log('birthyear is ' + typeof me.birthyear) ;
+console.log('ismarried is ' + typeof me.ismarried) ;
+console.log(me) ;
+console.log(me.studygroup,me.birthyear,me.ismarried,me.myname,me.surname,)
 
 let login = prompt('Please,enter your Nickname') ;                    //task 7
 let mail = prompt('Please,enter your E-mail') ;
